a216c17b231a57b894714a13ca54fd731d87b8ff
anonymous <snsokolov@users.noreply.github.com>
2018-04-08 02:25:05 -0700

    Created another minimal image based on the latest suse with no JDK pre-installed. Removed obsolete dockerfiles.

--- docker/test.sh
+++ docker/test.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/bash
 
-function finish { read -rp "Press Enter to exit..."; }
-
+echo -------- 1. Minimal image testing.
 IMAGE=minimal
 echo TEST: ${IMAGE}: Bazel image test started
 echo TEST: ${IMAGE}: Building the image...
-time docker build -t ${IMAGE} ${IMAGE} || finish
+time docker build -t ${IMAGE} ${IMAGE}
 echo TEST: ${IMAGE}: Image built done
 
-echo TEST FAILED
-finish
+echo -------- 2. Minimal_Java image testing.
+IMAGE=minimal_java
+echo TEST: ${IMAGE}: Bazel image test started
+echo TEST: ${IMAGE}: Building the image...
+time docker build -t ${IMAGE} ${IMAGE}
+echo TEST: ${IMAGE}: Image built done
 
+echo TEST PASSED
