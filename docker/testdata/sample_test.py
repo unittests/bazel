3e4c9ae8c8d11be669c0c652d9555d07606777ff
anonymous <snsokolov@users.noreply.github.com>
2018-04-08 02:40:36 -0700

    Testdata for docker images integration tests.

---
+++ docker/testdata/sample_test.py
@@ -0,0 +1,17 @@
+import unittest
+import sample
+
+
+class SampleTest(unittest.TestCase):
+
+
+  def testGiveMeAName(self):
+    pass
+
+
+  def testGiveMeAName2(self):
+    pass
+
+
+if __name__ == '__main__':
+  unittest.main()
